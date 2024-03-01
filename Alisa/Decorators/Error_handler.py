84104829de013d71ed78169fd06121ff5200e08d
VladSkopenko <skopirka2k17@gmail.com>
2024-03-01 20:42:11 +0300

    I added class name and error_handler

---
+++ Alisa/Decorators/Error_handler.py
@@ -0,0 +1,13 @@
+def error_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except ValueError as ve:
+            print(f"ValueError")
+        except AttributeError as a :
+            print(f"AttributeError")
+        except TypeError:
+            print(f"TypeError")
+    return wrapper
+
--- Alisa/Record_description_of_classes.py
+++ Alisa/Record_description_of_classes.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
+from Decorators.Error_handler import error_handler
 
 
 class Field(ABC):
+
     def __init__(self, value):
         self.value = value
 
@@ -9,4 +11,22 @@ class Field(ABC):
     def __str__(self):
         pass
 
+    @abstractmethod
+    def change_value(self, new_value):
+        pass
 
+
+class Name(Field):
+    def __init__(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise ValueError
+        super().__init__(value)
+
+    def __str__(self) -> str:
+        return f"{self.value}"
+
+    @error_handler
+    def change_value(self, new_name: str) -> None:
+        if not isinstance(new_name, str):
+            raise ValueError
+        self.value = new_name
