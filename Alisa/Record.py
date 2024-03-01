fb200c693ed28622af9edaaf3f31b7e02d1759c0
VladSkopenko <skopirka2k17@gmail.com>
2024-03-01 20:04:00 +0300

    I added abscrats class

--- Alisa/Record.py
+++ Alisa/Record.py
@@ -1,4 +1,12 @@
 from abc import ABC, abstractmethod
 
+
 class Field(ABC):
-    
+    def __init__(self, value):
+        self.value = value
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
