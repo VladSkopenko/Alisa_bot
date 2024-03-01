7d997a7740a3cf62eeab0ee2b6b710cdb8f4ea81
VladSkopenko <skopirka2k17@gmail.com>
2024-03-01 21:20:09 +0300

    Fixed getters and setters

--- Alisa/Record_description_of_classes.py
+++ Alisa/Record_description_of_classes.py
@@ -1,34 +1,32 @@
 from abc import ABC, abstractmethod
+
+from colorama import Fore
 from Decorators.Error_handler import error_handler
 
 
 class Field(ABC):
-
-    def __init__(self, value):
-        self._value = value
-
     @abstractmethod
     def __str__(self):
         pass
 
-
 class Name(Field):
     def __init__(self, value: str) -> None:
-        super().__init__(value)
         if not isinstance(value, str):
-            raise ValueError("Value must be a string")
-        self._value = value
+            raise ValueError(Fore.BLUE + "Value must be a string")
+        self.__value = value
 
     def __str__(self) -> str:
-        return f"{self._value}"
+        return f"{self.__value}"
 
     @property
-    def get_value(self) -> str:
-        return self._value
+    def value(self) -> str:
+        return self.__value
 
-    @get_value.setter
-    @error_handler
-    def get_value(self, new_name: str) -> None:
+    @value.setter
+    def value(self, new_name: str) -> None:
         if not isinstance(new_name, str):
-            raise ValueError()
-        self._value = new_name
+            raise ValueError(Fore.BLUE + "Value must be a string")
+        self.__value = new_name
+
+
+
