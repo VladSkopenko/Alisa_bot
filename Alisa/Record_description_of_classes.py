f1f4c01ce9acdeb001b4a0fb8550c6a64de18b48
VladSkopenko <skopirka2k17@gmail.com>
2024-03-01 21:04:28 +0300

    I added class 'name'

--- Alisa/Record_description_of_classes.py
+++ Alisa/Record_description_of_classes.py
@@ -5,28 +5,30 @@ from Decorators.Error_handler import error_handler
 class Field(ABC):
 
     def __init__(self, value):
-        self.value = value
+        self._value = value
 
     @abstractmethod
     def __str__(self):
         pass
 
-    @abstractmethod
-    def change_value(self, new_value):
-        pass
-
 
 class Name(Field):
     def __init__(self, value: str) -> None:
-        if not isinstance(value, str):
-            raise ValueError
         super().__init__(value)
+        if not isinstance(value, str):
+            raise ValueError("Value must be a string")
+        self._value = value
 
     def __str__(self) -> str:
-        return f"{self.value}"
+        return f"{self._value}"
 
+    @property
+    def get_value(self) -> str:
+        return self._value
+
+    @get_value.setter
     @error_handler
-    def change_value(self, new_name: str) -> None:
+    def get_value(self, new_name: str) -> None:
         if not isinstance(new_name, str):
-            raise ValueError
-        self.value = new_name
+            raise ValueError()
+        self._value = new_name
