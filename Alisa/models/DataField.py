c945b56eff514cbcbf94f8b92aee05e8fb8d6655
VladSkopenko <skopirka2k17@gmail.com>
2024-03-24 01:08:08 +0300

    create data Fields

---
+++ Alisa/models/DataField.py
@@ -0,0 +1,13 @@
+from Field import Field
+from colorama import Fore
+
+
+class DataField(Field):
+    """
+    Represents a field in a data record for name, address, company, or tag.
+    """
+
+    def validate(self, new_value: str) -> str:
+        if not isinstance(new_value, str):
+            raise ValueError(Fore.BLUE + "Value must be a string")
+        return new_value.title()
