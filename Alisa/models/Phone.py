891543ab678a705bcb5b8c2b02a6df0ecd007cbe
VladSkopenko <skopirka2k17@gmail.com>
2024-03-24 01:09:17 +0300

    + phone

---
+++ Alisa/models/Phone.py
@@ -0,0 +1,26 @@
+from re import match
+
+from colorama import Fore
+
+from Field import Field
+
+
+class Phone(Field):
+    """
+    Represents a field for phone numbers and provides validation.
+    """
+
+    def validate(self, phone_number: str | list[str, ...]) -> str | list[str, ...]:
+        if isinstance(phone_number, str):
+            pattern = r"^\+?380([0-9]{9}$)|0([0-9]{9}$)"
+            if match(pattern, phone_number):
+                return phone_number
+            else:
+                raise ValueError(Fore.BLUE + "Invalid phone number")
+        elif isinstance(phone_number, list):
+            validated_numbers = []
+            for number in phone_number:
+                validated_numbers.append(self.validate(number))
+            return validated_numbers
+        else:
+            raise ValueError("Phone must be a string or a list of strings")
