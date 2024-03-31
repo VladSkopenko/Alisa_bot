8c5d6e1ebf232b88a43d162a4eb0948362f39269
VladSkopenko <skopirka2k17@gmail.com>
2024-04-01 02:10:17 +0300

    create remove bot

--- Alisa/Bot.py
+++ Alisa/Bot.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
 from models.record import Record
-from Address_book import ADDRESSBOOK
-from Connect_db import update_db
+from Address_book import AddressBook
+from models.RecordDocument import RecordDocument
+
 
 class AbstractBot(ABC):
     def __init__(self):
-        self.address_book = ADDRESSBOOK
+        self.address_book = AddressBook()
 
     @abstractmethod
     def handle(self):
@@ -13,16 +14,32 @@ class AbstractBot(ABC):
 
 
 class AddBot(AbstractBot):
-    def __init__(self):
-        super().__init__()
 
     def handle(self, record: Record) -> None:
-        update_db(record)
-
+        """
+        Add a contact to the database
+        """
+        contact_for_db = RecordDocument(
+            name=str(record.name),
+            phone=[str(phone) for phone in record.phone],
+            email=str(record.email),
+            tag=[str(tag) for tag in record.tags],
+            address=str(record.address),
+            company=str(record.company),
+            birthday=str(record.birthday),
+        )
+        contact_for_db.save()
 
 
 class RemoveBot(AbstractBot):
-    ...
+
+    def handle(self, record_id) -> None:
+        """
+        Delete a record from the database based on its ID.
+        """
+        record = RecordDocument.objects(id=record_id).first()
+        if record:
+            record.delete()
 
 
 class EditBot(AbstractBot):
@@ -35,14 +52,13 @@ class FindBot(AbstractBot):
 
 if __name__ == "__main__":
     ad = AddBot()
-    per = Record(name="Vlad",
+    per = Record(name="Liuda",
                  phone="380961630573",
-                 tag="student",
+                 tag="mentor",
                  email="exsam@fa.com",
-                 birthday="2000-01-28",
-                 company="go it",
+                 birthday="1990-12-18",
+                 company="soft",
                  address="address231213"
                  )
-    ad.handle(per)
-
-
+    rm = RemoveBot()
+    rm.handle("6609ecc0db3f7ece52a472ca")
