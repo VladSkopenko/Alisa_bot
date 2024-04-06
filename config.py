15c4faa379a3ef2ba92ed4fcca883d39c5c7e02b
VladSkopenko <skopirka2k17@gmail.com>
2024-04-06 15:23:20 +0300

    create yml and require update

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
-from pydantic import EmailStr
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     POSTGRES_URL: str
+    MONGO_URL: str
     REDIS_DOMAIN: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_PASSWORD: str = None
-    CLOUDINARY_NAME: str
-    CLOUDINARY_API_KEY: int
-    CLOUDINARY_API_SECRET: str = "secret"
 
     class Config:
         extra = "ignore"
