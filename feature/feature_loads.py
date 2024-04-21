7c3521d7f4c23b4607a639038473e6ea8fe1f570
VladSkopenko <skopirka2k17@gmail.com>
2024-04-21 15:28:56 +0300

    crate new feature

---
+++ feature/feature_loads.py
@@ -0,0 +1,7 @@
+import tqdm
+import time
+
+
+
+for _ in tqdm.tqdm(range(100)):
+    time.sleep(0.25)
---
+++ feature/feature_p.py
@@ -0,0 +1,5 @@
+from art import tprint
+from termcolor import colored
+
+tprint("Welcome", font="block", chr_ignore=True)
+tprint("Alisa", "white_bubble")
